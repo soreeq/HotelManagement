e4ff1a3546cecae0746a347d824df4dfb4268c7d
Grzesiek Szawara <soreeq@gmail.com>
2021-12-28 13:20:18 +0100

    frontend logowania

--- src/main/java/pl/hotelmanagement/guests/GuestService.java
+++ src/main/java/pl/hotelmanagement/guests/GuestService.java
@@ -47,7 +47,7 @@ public class GuestService {
     @GetMapping("/add-guest")
     public String showAddGuestForm(Model model){
         model.addAttribute("newGuest", new Guest());
-        return "guests";
+        return "addguest";
     }
     @PostMapping("/add-guest")
     public String addGuest(/*@RequestHeader("username") String username,*/ @ModelAttribute Guest guest){
